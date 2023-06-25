2191181300571f094758baa7445ca71d156bb4c2
iamcoolalan <iamalan1996@gmail.com>
2023-06-25 15:42:37 +0800

    feat: add facebook login

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,8 @@
 const passport = require('passport')
-const LocalStrategy = require('passport-local').Strategy
 const bcrypt = require('bcryptjs')
+
+const LocalStrategy = require('passport-local').Strategy
+const FacebookStrategy = require('passport-facebook').Strategy
 
 const User = require('../models/user')
 
@@ -9,16 +11,16 @@ module.exports = app => {
   app.use(passport.session());
 
   passport.use(new LocalStrategy(
-    { usernameField: 'email', passReqToCallback: true }, 
+    { usernameField: 'email', passReqToCallback: true },
     (req, email, password, done) => {
       User.findOne({ email })
         .then(user => {
-          if(!user){
+          if (!user) {
             return done(null, false, req.flash('login_warning_msg', '此Email還未註冊'))
           }
           return bcrypt.compare(password, user.password)
             .then(isMatch => {
-              if(!isMatch){
+              if (!isMatch) {
                 return done(null, false, req.flash('login_warning_msg', 'Email或密碼錯誤!'))
               }
               return done(null, user)
@@ -27,6 +29,39 @@ module.exports = app => {
         .catch(err => done(err, false))
     }
   ));
+
+  passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_ID,
+    clientSecret: process.env.FACEBOOK_SECRET,
+    callbackURL: process.env.FACEBOOK_CALLBACK,
+    profileFields: ['email', 'displayName']
+  }, (accessToken, refreshToken, profile, done) => {
+    const { name, email } = profile._json
+
+    User.findOne({ email })
+      .then(user => {
+        if (user) {
+          return done(null, user)
+        }
+
+        const randomPassword = Math.random().toString(36).slice(-8)
+
+        bcrypt
+          .genSalt(10)
+          .then(salt => bcrypt.hash(randomPassword, salt))
+          .then(hash => {
+            return User.create({
+              name,
+              email,
+              password: hash
+            })
+          })
+          .then(user => done(null, user))
+          .catch(err => done(err, false))
+      })
+      .catch(err => done(err, false))
+
+  }))
 
   passport.serializeUser(function (user, done) {
     done(null, user.id);
--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,13 @@ const router = express.Router()
 const home = require('./modules/home')
 const record = require('./modules/record')
 const user = require('./modules/user')
+const auth = require('./modules/auth')
 
 const { authenticator } = require('../middleware/auth')
 
 router.use('/record', authenticator, record)
 router.use('/users', user)
+router.use('/auth', auth)
 router.use('/', authenticator, home)
 
 module.exports = router
