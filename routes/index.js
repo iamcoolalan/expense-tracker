d622efc8a00dc942b9b4951c43b9cce4278d6815
iamcoolalan <iamalan1996@gmail.com>
2023-07-01 20:13:34 +0800

    refactor: let routes follow principles of RESTful design

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const router = express.Router()
 
-const home = require('./modules/home')
 const record = require('./modules/record')
 const user = require('./modules/user')
 const auth = require('./modules/auth')
@@ -11,6 +10,8 @@ const { authenticator } = require('../middleware/auth')
 router.use('/record', authenticator, record)
 router.use('/users', user)
 router.use('/auth', auth)
-router.use('/', authenticator, home)
+router.use('/', (req, res ) => {
+  res.redirect('/record')
+})
 
 module.exports = router
--- routes/modules/record.js
+++ routes/modules/record.js
@@ -5,8 +5,110 @@ const Record = require('../../models/record')
 const Category = require('../../models/category')
 const mongoose  = require('mongoose')
 
+const functions = require('../../utils/functions')
+
+//Read
+//render all user's record
+router.get('/', async (req, res) => {
+  try {
+    const userId = req.user._id
+
+    const records = await Record.aggregate([
+      //$match 等等.. 錢字號後面，代表mongoDB的資料庫指令
+      //match 找出符合條件的資料
+      { $match: { userId } },
+      //sort 排列資料
+      { $sort: { date: -1 } },
+      //lookup 將categories 表格join進records，讓後續可以操作將categories的欄位
+      {
+        $lookup: {
+          from: 'categories',
+          localField: 'categoryId', //當前資料的欄位
+          foreignField: '_id', // 要關聯資料的欄位
+          as: 'category'
+        }
+      },
+      //addFields 新增欄位
+      {
+        $addFields: {
+          date: { $dateToString: { format: '%Y/%m/%d', date: '$date' } }, //因為名字也取date，會覆蓋原本date欄位的內容
+          icon: { $arrayElemAt: ['$category.icon', 0] }
+        }
+      },
+      //project 最後要取用的欄位，0表示不取用，1表示取用
+      {
+        $project: {
+          _id: 1,
+          name: 1,
+          date: 1,
+          amount: 1,
+          icon: 1,
+
+        }
+      }
+    ])
+
+    functions.style(records)
+    const totalAmount = functions.sum(records)
+    const categories = await Category.find().lean()
+
+    res.status(200).render('index', { categories, records, totalAmount })
+  } catch (err) {
+    console.log(err)
+    res.status(500).send('An error occurred')
+  }
+
+})
+
+//render record by filter
+router.post('/filter', async (req, res) => {
+  try {
+    const categoryName = req.body.category
+    const userId = req.user._id
+
+    const records = await Record.aggregate([
+      {
+        $lookup: {
+          from: 'categories',
+          localField: 'categoryId',
+          foreignField: '_id',
+          as: 'category'
+        }
+      },
+      { $match: { 'category.name': categoryName, userId } },
+      { $sort: { date: -1 } },
+      {
+        $addFields: {
+          date: { $dateToString: { format: '%Y/%m/%d', date: '$date' } },
+          icon: { $arrayElemAt: ['$category.icon', 0] }
+        }
+      },
+      {
+        $project: {
+          _id: 1,
+          name: 1,
+          date: 1,
+          amount: 1,
+          icon: 1
+        }
+      }
+    ])
+
+    functions.style(records)
+    const totalAmount = functions.sum(records)
+    const categories = await Category.find().lean()
+
+    res.status(200).render('index', { categories, categoryName, records, totalAmount })
+
+  } catch (err) {
+    console.log(err)
+    res.status(500).send('An error occurred')
+  }
+})
+
 
 //Create
+//render create page
 router.get('/new', (req, res) => {
   Category.find()
     .lean()
@@ -14,6 +116,7 @@ router.get('/new', (req, res) => {
     .catch(err => console.log(err))
 })
 
+//create new record
 router.post('/new', async (req, res) => {
   try {
     const newRecord = req.body
@@ -41,6 +144,7 @@ router.post('/new', async (req, res) => {
 })
 
 //Edit
+//render edit page
 router.get('/:id/edit', async (req, res) => {
   try {
     const _id = new mongoose.Types.ObjectId(req.params.id)
@@ -95,6 +199,7 @@ router.get('/:id/edit', async (req, res) => {
   }
 })
 
+//update record
 router.put('/:id/edit', async (req, res) => {
   try {
     const _id = req.params.id
@@ -129,6 +234,7 @@ router.put('/:id/edit', async (req, res) => {
 })
 
 //Delete
+//delete record
 router.delete('/:id', (req, res) => {
   const _id = req.params.id
   const userId = req.user._id
